46ed1889d9e2e61523326488dd23432afbf7cafd
Shantanu Oak <shantanu.oak@gmail.com>
2019-06-11 18:22:22 +0530

    Add files via upload

---
+++ dicts/process_file.py
@@ -0,0 +1,11 @@
+﻿with open('mr_IN4.dic', 'w+') as d:
+    with open('mr_IN3.dic') as f:
+        for i in list(f)[:10000]:
+            try:
+                if i.rstrip('\n').endswith('णे'):
+                    d.write(i+'\n')
+                else:
+                    d.write(i.rstrip('\n') + '/P' + '\n')
+            except:
+                pass
+            
